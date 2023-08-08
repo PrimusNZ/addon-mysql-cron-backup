4e4b3739abebeddf9bb3b083778b11687df9904a
PrimusNZ <primus@primusnz.live>
2023-08-08 22:24:02 +1200

    Update

--- backup.sh
+++ backup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Get hostname: try read from file, else get from env
-[ -z "${MYSQL_HOST_FILE}" ] || { MYSQL_HOST=$(head -1 "${MYSQL_HOST_FILE}"); }
+[ -z "$(bashio::config mysql_host)" ] || { MYSQL_HOST=$(bashio::config mysql_host); }
 [ -z "${MYSQL_HOST}" ] && { echo "=> MYSQL_HOST cannot be empty" && exit 1; }
 # Get username: try read from file, else get from env
-[ -z "${MYSQL_USER_FILE}" ] || { MYSQL_USER=$(head -1 "${MYSQL_USER_FILE}"); }
+[ -z "$(bashio::config mysql_user)" ] || { MYSQL_USER=$(bashio::config mysql_user); }
 [ -z "${MYSQL_USER}" ] && { echo "=> MYSQL_USER cannot be empty" && exit 1; }
 # Get password: try read from file, else get from env, else get from MYSQL_PASSWORD env
-[ -z "${MYSQL_PASS_FILE}" ] || { MYSQL_PASS=$(head -1 "${MYSQL_PASS_FILE}"); }
+[ -z "$(bashio::config mysql_pass)" ] || { MYSQL_PASS=$(bashio::config mysql_pass); }
 [ -z "${MYSQL_PASS:=$MYSQL_PASSWORD}" ] && { echo "=> MYSQL_PASS cannot be empty" && exit 1; }
 # Get database name(s): try read from file, else get from env
 # Note: when from file, there can be one database name per line in that file
-[ -z "${MYSQL_DATABASE_FILE}" ] || { MYSQL_DATABASE=$(cat "${MYSQL_DATABASE_FILE}"); }
+[ -z "$(bashio::config mysql_db)" ] || { MYSQL_DATABASE=$(bashio::config mysql_db); }
 # Get level from env, else use 6
-[ -z "${GZIP_LEVEL}" ] && { GZIP_LEVEL=6; }
+[ -z "$(bashio::config gzip_level)" ] && { GZIP_LEVEL=6; }
 
 DATE=$(date +%Y%m%d%H%M)
 echo "=> Backup started at $(date "+%Y-%m-%d %H:%M:%S")"
@@ -27,9 +27,9 @@ do
       && [[ "$db" != _* ]]
   then
     echo "==> Dumping database: $db"
-    FILENAME=/backup/$DATE.$db.sql
-    LATEST=/backup/latest.$db.sql
-    if mysqldump --single-transaction $MYSQLDUMP_OPTS -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" $MYSQL_SSL_OPTS "$db" > "$FILENAME"
+    FILENAME="$(bashio::config backup_folder)/$DATE.$db.sql"
+    LATEST="$(bashio::config backup_folder)/latest.$db.sql"
+    if mysqldump --single-transaction $(bashio::config mysqldump_opts) -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" $MYSQL_SSL_OPTS "$db" > "$FILENAME"
     then
       EXT=
       if [ -z "${USE_PLAIN_SQL}" ]
@@ -43,7 +43,7 @@ do
       BASENAME=$(basename "$FILENAME")
       echo "==> Creating symlink to latest backup: $BASENAME"
       rm "$LATEST" 2> /dev/null
-      cd /backup || exit && ln -s "$BASENAME" "$(basename "$LATEST")"
+      cd "$(bashio::config backup_folder)" || exit && ln -s "$BASENAME" "$(basename "$LATEST")"
       if [ -n "$MAX_BACKUPS" ]
       then
         # Execute the delete script, delete older backup or other custom delete script
--- delete.sh
+++ delete.sh
@@ -5,9 +5,9 @@ EXT=$2
 
 # This file could be customized to create custom delete strategy
 
-while [ "$(find /backup -maxdepth 1 -name "*.$db.sql$EXT" -type f | wc -l)" -gt "$MAX_BACKUPS" ]
+while [ "$(find "$(bashio::config backup_folder)" -maxdepth 1 -name "*.$db.sql$EXT" -type f | wc -l)" -gt "$(bashio::config max_backups)" ]
 do
-  TARGET=$(find /backup -maxdepth 1 -name "*.$db.sql$EXT" -type f | sort | head -n 1)
+  TARGET=$(find "$(bashio::config backup_folder)" -maxdepth 1 -name "*.$db.sql$EXT" -type f | sort | head -n 1)
   echo "==> Max number of ($MAX_BACKUPS) backups reached. Deleting ${TARGET} ..."
   rm -rf "${TARGET}"
   echo "==> Backup ${TARGET} deleted"
